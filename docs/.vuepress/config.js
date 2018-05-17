2072346023941cafefffcff0c0c0aa84d8982c86
andrea <chiabotto@newsaktuell.de>
2018-05-17 10:43:37 +0200

    navigation with #component# in the right place

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,43 +1,46 @@
 module.exports = {
-  base: "/design-kit/",
-  themeConfig: {
-    nav: false,
-    sidebar: [
-      {
-        title: "Über das Design Kit",
-        children: [
-          "./connect-design-kit/",
-          "./connect-design-kit/was-wie-wieso"
+    base: "/design-kit/",
+    themeConfig: {
+        nav: false,
+        sidebar: [
+            {
+                title: "Über das Design Kit",
+                children: [
+                    "./connect-design-kit/",
+                    "./connect-design-kit/was-wie-wieso"
+                ]
+            },
+            {
+                title: "Grundlagen",
+                children: [
+                    "./grundlagen/",
+                    "./grundlagen/farben",
+                    "./grundlagen/schriftart"
+                ]
+            },
+            {
+                title: "Komponenten",
+                children: [
+                    "./komponenten/form",
+                    "./komponenten/buttons"
+                ]
+            },
+            {
+                title: "Wertenheiten und akteuren",
+                children: [
+                    "./wertenheiten-und-akteuren/",
+                    "./wertenheiten-und-akteuren/nutzer",
+                    "./wertenheiten-und-akteuren/mitarbeiter",
+                    "./wertenheiten-und-akteuren/team",
+                    "./wertenheiten-und-akteuren/tool",
+                    "./wertenheiten-und-akteuren/material",
+                    "./wertenheiten-und-akteuren/medienformat"
+                ]
+            },
+            {
+                title: "Interaktionen",
+                children: ["./interaktionen/anmelden-mit-dpa-id"]
+            }
         ]
-      },
-      {
-        title: "Grundlagen",
-        children: [
-          "./grundlagen/",
-          "./grundlagen/farben",
-          "./grundlagen/schriftart"
-        ]
-      },
-      {
-        title: "Wertenheiten und akteuren",
-        children: [
-          "./wertenheiten-und-akteuren/",
-          "./wertenheiten-und-akteuren/nutzer",
-          "./wertenheiten-und-akteuren/mitarbeiter",
-          "./wertenheiten-und-akteuren/team",
-          "./wertenheiten-und-akteuren/tool",
-          "./wertenheiten-und-akteuren/material",
-          "./wertenheiten-und-akteuren/medienformat"
-        ]
-      },
-      {
-        title: "Interaktionen",
-        children: ["./interaktionen/anmelden-mit-dpa-id"]
-      },
-      {
-        title: "Komponenten",
-        children: ["./komponenten/"]
-      }
-    ]
-  }
+    }
 };
