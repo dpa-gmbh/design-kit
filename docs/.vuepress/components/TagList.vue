6bf9eae818322bd2d1c64405b329bf67d703b755
Michael Schieben <babe@rockitbaby.de>
2018-05-17 12:22:44 +0200

    add synonym and taglist

---
+++ docs/.vuepress/components/TagList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cdk-TagList">
+    <ul>
+      <li class="cdk-TagList-item" v-for="item in items">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    }
+  }
+}
+</script>
+<style>
+/** @define cdk-List */
+
+.cdk-TagList {
+  
+}
+
+.cdk-TagList ul {
+  padding-left: 0;
+}
+
+.cdk-TagList-item {
+  color: #2294fe;
+  font-size: .8em;
+  display: inline-block;
+  margin: 0;
+}
+
+.cdk-TagList-item::after {
+  content: "•";
+  padding: 0 4px;
+}
+
+.cdk-TagList-item:last-child::after {
+  content: none;
+}
+</style>
