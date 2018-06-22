2bfb0a03cb3af170ea01f240ecdeb4a13179009c
Michael Schieben <babe@rockitbaby.de>
2018-06-22 20:24:27 +0200

    fix indentation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,75 +1,74 @@
 module.exports = {
-    base: "/design-kit/",
-    title: "dpa-Connect Design Kit",
-    head: [
-        [
-            'script',
-            {
-                src: '/preview.js'
-            }
-        ],
-        [
-            'script',
-            {
-                src: 'https://unpkg.com/@webcomponents/webcomponentsjs@2.0.0/webcomponents-loader.js'
-            }
-        ],
-        [
-            'script',
-            {
-                src: 'https://unpkg.com/vue'
-            }
-        ],
-        [
-            'script',
-            {
-                src: 'https://unpkg.com/@dpa-connect/user@0.5.0/dist/cdk-user.min.js'
-            }
-        ],
+  base: "/design-kit/",
+  title: "dpa-Connect Design Kit",
+  head: [
+    [
+      'script',
+      {
+        src: '/preview.js'
+      }
     ],
-    themeConfig: {
-        sidebar: [
-            {
-                title: "Grundlagen",
-                children: [
-                    "./grundlagen/farben",
-                    "./grundlagen/schriftart"
-                ]
-            },
-            {
-                title: "Komponenten",
-                children: [
-                    "./komponenten/formular",
-                    "./komponenten/button",
-                    "./komponenten/beispiel"
-                ]
-            },
-            {
-                title: "Werteinheiten und Akteure",
-                children: [
-                    "./werteinheiten-und-akteure/filter",
-                    "./werteinheiten-und-akteure/inhalt",
-                    "./werteinheiten-und-akteure/nutzer",
-                    "./werteinheiten-und-akteure/medienformat",
-                    "./werteinheiten-und-akteure/mitarbeiter",
-                    "./werteinheiten-und-akteure/team",
-                    "./werteinheiten-und-akteure/tool"
-                ]
-            },
-            {
-                title: "Interaktionen",
-                children: [
-                    "./interaktionen/anmelden-mit-dpa-id",
-                    "./interaktionen/wiederholt-relevantes-finden",
-                ]
-            },
-            {
-                title: "Appendix",
-                children: [
-                    "./connect-design-kit/glossar",
-                    "./connect-design-kit/prozess"
-                ]
-            },
+    [
+      'script',
+      {
+        src: 'https://unpkg.com/@webcomponents/webcomponentsjs@2.0.0/webcomponents-loader.js'
+      }
+    ],
+    [
+      'script',
+      {
+        src: 'https://unpkg.com/vue'
+      }
+    ],
+    [
+      'script',
+      {
+        src: 'https://unpkg.com/@dpa-connect/user@0.5.0/dist/cdk-user.min.js'
+      }
+    ],
+  ],
+  themeConfig: {
+    sidebar: [{
+        title: "Grundlagen",
+        children: [
+          "./grundlagen/farben",
+          "./grundlagen/schriftart"
         ]
-    }
+      },
+      {
+        title: "Komponenten",
+        children: [
+          "./komponenten/formular",
+          "./komponenten/button",
+          "./komponenten/beispiel"
+        ]
+      },
+      {
+        title: "Werteinheiten und Akteure",
+        children: [
+          "./werteinheiten-und-akteure/filter",
+          "./werteinheiten-und-akteure/inhalt",
+          "./werteinheiten-und-akteure/nutzer",
+          "./werteinheiten-und-akteure/medienformat",
+          "./werteinheiten-und-akteure/mitarbeiter",
+          "./werteinheiten-und-akteure/team",
+          "./werteinheiten-und-akteure/tool"
+        ]
+      },
+      {
+        title: "Interaktionen",
+        children: [
+          "./interaktionen/anmelden-mit-dpa-id",
+          "./interaktionen/wiederholt-relevantes-finden",
+        ]
+      },
+      {
+        title: "Appendix",
+        children: [
+          "./connect-design-kit/glossar",
+          "./connect-design-kit/prozess"
+        ]
+      },
+    ]
+  }
 };
